4a81035513f7b81c5f9f1105c86c9abab1c653ba
Iryna <batsianiryna@gmail.com>
2022-12-12 16:05:40 +0300

    add task создание двумерного массива с вводом в одну строку

---
+++ Task/Program.cs
@@ -0,0 +1,8 @@
+﻿// Ввод данных в одну строку
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+int row = int.Parse(numbers[0]);
+int col = int.Parse(numbers[1]);
+int[,] matrix = new int[row, col]; // Создание двумерного массива
