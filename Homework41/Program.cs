7041f1bf86b14df7f090173b57c53df37f369e5a
Iryna <batsianiryna@gmail.com>
2022-12-12 16:29:33 +0300

    add homework41 как проще решить дом.задачу

---
+++ Homework41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 41. Пользователь вводит n чисел с клавиатуры. Нужно посчитать, сколько чисел > 0 ввел пользователь.
+
+Console.Clear();
+Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+  if (Convert.ToInt32(Console.ReadLine()) > 0)
+  count++;
+}
+Console.WriteLine(count);
